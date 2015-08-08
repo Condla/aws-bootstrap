349ac4d7ec5eaed14b815c624d0d65db232ea3a7
Stefan Kupstaitis-Dunkler <stefan.dun@gmail.com>
2015-08-08 23:49:01 +0200

    restructuring of dirs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # aws-bootstrap
-Fast creating and termination of new (micro) instances for tinkering purposes
+Fast creating and termination of new (micro) instances for tinkering purposes.
+./redhat/createInstances.sh is configured to create a RedHat7 Single-Node Hadoop Cluster using Ambari out of the box.
--- ubuntu_startUp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-export AMBARI_REPO="http://s3.amazonaws.com/dev.hortonworks.com/ambari/ubuntu12/2.x/BUILDS/2.1.0-1409/ambaribn.list "
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install wget
-cd /etc/apt/sources.list.d
-sudo wget $AMBARI_REPO 
-sudo apt-get -y install ambari-server ambari-agent
